4a08f167ffd601f79db0e004ecf0e327757b8b1a
Your Name <you@example.com>
2019-05-10 19:44:24 -0300

    criei os models

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    registration = models.CharField(max_length=10)
+    year = models.IntegerField()
+    genre = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    shift = models.CharField(max_length=50)
+    
+
+    def __str__(self):
+        return self.name
+    
+class Class(models.Model):
+    name = models.CharField(max_length=50)
+    student_quantity = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
+    
