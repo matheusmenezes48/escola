91562ae0d0c3e5b540aafaa2e5b02b457b7c6523
Your Name <you@example.com>
2019-05-17 21:33:23 -0300

    listas quase completas

--- escola/models.py
+++ escola/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Course(models.Model):
     name = models.CharField(max_length=50)
+    modality = models.CharField(max_length=50)
+    shift = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
