f9f9a62c058a4174e101941f64d5a44ca65527d6
enoquemoais <enoquemorais2010@gmail.com>
2019-05-18 20:42:17 -0300

    filter

--- escola/views.py
+++ escola/views.py
@@ -5,13 +5,36 @@ from . models import Student,Course,Class
 def home(request):
     return render(request, 'home.html',{})
 
+
+
+
 def student(request):
-    students = Student.objects.all()
+    registration = request.GET.get('registration',None)
+
+    if registration:
+        students = Student.objects.all()
+
+        students = students.filter(registration__icontains=registration) 
+    else:
+        students = Student.objects.all()
+
     return render(request, 'student/list.html', {'students':students})
 
+
+
+
 def course(request):
+     name = request.GET.get('name',None)
+     if name:
+        courses = Course.objects.all()
+        courses = courses.filter(name__icontains=name) 
+     else:
+        courses = Course.objects.all()
+     return render(request, 'course/list.html', {'courses':courses})
+
+'''def course(request):
     courses = Course.objects.all()
-    return render(request, 'course/list.html', {'courses':courses})
+    return render(request, 'course/list.html', {'courses':courses})'''
 
 def classs(request):
     classs = Class.objects.all()
